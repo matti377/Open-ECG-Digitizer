fix: fill average rgb image color for perspective

src/model/cropper.py
@@ -1,5 +1,5 @@
 import torch
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List
 from torchvision.transforms.functional import perspective
 
 
@@ -237,7 +237,7 @@ class Cropper(torch.nn.Module):
         return -y * torch.tan(theta) + rho / torch.cos(theta)
 
     def apply_perspective(
-        self, input_tensor: torch.Tensor, source_points: torch.Tensor, fill_value: float = 0
+        self, input_tensor: torch.Tensor, source_points: torch.Tensor, fill_value: float | List[float] = 0
     ) -> torch.Tensor:
         original_shape = input_tensor.shape
         if input_tensor.ndim == 2:  # (H, W)

src/model/inference_wrapper.py
@@ -36,8 +36,9 @@ class InferenceWrapper(torch.nn.Module):
 
         source_points = self.cropper(signal_probabilities, alignment_params)
 
+        mean_image_color = image.mean(dim=(0, 2, 3))
         aligned_signal_probabilities = self.cropper.apply_perspective(signal_probabilities, source_points, fill_value=0)
-        aligned_image = self.cropper.apply_perspective(image, source_points, fill_value=image.mean().item())
+        aligned_image = self.cropper.apply_perspective(image, source_points, fill_value=mean_image_color.tolist())
 
         # TODO: unit detection
 